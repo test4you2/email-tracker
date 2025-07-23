a562ef655b8f30d68063e007e4f162d8b1f1dd47
test4you2 <test4you2222@gmail.com>
2025-07-23 16:32:16 +0530

    Update pixel.js

--- api/pixel.js
+++ api/pixel.js
@@ -2,13 +2,20 @@ export default async function handler(req, res) {
   const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   const ua = req.headers['user-agent'];
   const query = req.query;
-  const time = new Date().toISOString();
 
-  console.log(`\n[EMAIL OPENED]`);
-  console.log(`Time: ${time}`);
-  console.log(`IP: ${ip}`);
-  console.log(`User Agent: ${ua}`);
-  console.log(`Query Params:`, query);
+  const logPayload = {
+    type: "open",
+    email: query.email || "",
+    campaign: query.campaign || "",
+    ip,
+    ua,
+  };
+
+  await fetch("https://script.google.com/macros/s/AKfycbzwDruR-Eh06iWS-tMnims3n2IyRzQ4G5vwpvCUmCPHy8_3Ksgz15M8r8aS0PPmXm4t/exec", {
+    method: "POST",
+    body: JSON.stringify(logPayload),
+    headers: { "Content-Type": "application/json" },
+  });
 
   const img = Buffer.from('R0lGODlhAQABAAAAACwAAAAAAQABAAA=', 'base64');
   res.setHeader('Content-Type', 'image/gif');
